891eeefe4c19a0def59705d796c552c300a0e674
Banwind <a84012807@gmail.com>
2023-06-01 17:07:25 +0800

    feat: add api error handler & delete restaurant api

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,5 +7,19 @@ module.exports = {
     }
     res.redirect('back')
     next(err)
+  },
+  apiErrorHandler (err, req, res, next) {
+    if (err instanceof Error) {
+      res.status(err.status || 500).json({
+        message: `${err.name}: ${err.message}`,
+        status: 'error'
+      })
+    } else {
+      res.status(500).json({
+        message: `${err}`,
+        status: 'error'
+      })
+    }
+    next(err)
   }
 }
